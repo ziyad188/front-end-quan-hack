86bcaa784a8836d84b9ee29010653c6886a713fe
mohammed ziyad <mohammedziyad@Ziyads-MacBook-Air.local>
2024-04-01 21:52:03 +0530

    intial upload

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const handleButtonClick = () => {
+    axios.get('http://127.0.0.1:5002')
+      .then(response => {
+        console.log(response.data); 
+        document.getElementById('response').innerText = response.data; 
+      })
+      .catch(error => {
+        console.error('Error sending request to Flask App 1:', error);
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome</h1>
+        <button onClick={handleButtonClick}>Send Request</button>
+        <h1 id="response"></h1> 
       </header>
     </div>
   );
